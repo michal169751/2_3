a1e297103259d6ff7c41397b5f52a8352a4f9abf
michal169751 <127328268+michal169751@users.noreply.github.com>
2023-03-22 16:08:53 +0000

    2_3

--- Main.java
+++ Main.java
@@ -4,13 +4,10 @@ class Main {
   public static void main(String[] args)
   {
     Random generator = new Random();
-    for (int i=0; i<5; i++)
-      {
-        double a = generator.nextInt(9);
-        double b = generator.nextInt(9);
-        double c = generator.nextInt(9);
-        double d = generator.nextInt(9);
-        double e = generator.nextInt(9);
-      }
+    double a = generator.nextInt(9);
+    double b = generator.nextInt(9);
+    double c = generator.nextInt(9);
+    double d = generator.nextInt(9);
+    double e = generator.nextInt(9);
   }
 }
