bd6f12f8d654a44815071f733683939e6d6c386b
michal169751 <127328268+michal169751@users.noreply.github.com>
2023-03-22 16:05:17 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.lang.Math;
+class Main {
+  public static void main(String[] args)
+  {
+    Random generator = new Random();
+    for (int i=0; i<5; i++)
+      {
+        double a = generator.nextInt(9);
+        double b = generator.nextInt(9);
+        double c = generator.nextInt(9);
+        double d = generator.nextInt(9);
+        double e = generator.nextInt(9);
+      }
+  }
+}
