cd25cd273483cef58cfecda176073613079e528f
michal169751 <>
2023-04-04 17:10:36 +0000

    2_3

--- Main.java
+++ Main.java
@@ -1,13 +1,24 @@
 import java.util.Random;
 import java.lang.Math;
-class Main {
-  public static void main(String[] args)
-  {
-    Random gen = new Random();
-    int a = gen.nextInt(9);
-    int b = gen.nextInt(9);
-    int c = gen.nextInt(9);
-    int d = gen.nextInt(9);
-    int e = gen.nextInt(9);
-  }
+
+public class Main {
+    public static void main(String[] args) {
+        Random gen = new Random();
+        int suma = 0;
+        int iloczyn = 1;
+        double potega = 0;
+        
+        System.out.print("Losowe liczby: ");
+        for (int i = 0; i < 5; i++) {
+            int liczby = gen.nextInt(10);
+            System.out.print(liczby + " ");
+            suma = suma + liczby;
+            iloczyn = iloczyn * liczby;
+            potega = potega + Math.pow(liczby,2);
+        }
+        
+        System.out.println("\nSuma: " + suma);
+        System.out.println("Iloczyn: " + iloczyn);
+        System.out.println("Potęga: " + potega);
+    }
 }
